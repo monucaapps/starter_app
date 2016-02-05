1322196b75114843f5e201800cdf036e25d8edee
monucaapps <monucaapps@gmail.com>
2016-02-05 22:17:45 +0000

    improve add hello action route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   
-  root 'aplication#hello'
+  root 'application#hello'
   
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
